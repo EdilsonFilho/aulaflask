d8ccf3d9231fb1f5ef499f171a3c18d48d57f328
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-08-11 17:57:13 -0300

    criando projeto usando flask

---
+++ db.py
@@ -0,0 +1,11 @@
+users={
+    "edilson":{
+        "name": "Edilson Filho",
+        "tel": "123-456-789"
+    },
+    "maria":{
+        "name": "Maria ",
+        "tel": "987-6564- 789"
+    }
+
+}
